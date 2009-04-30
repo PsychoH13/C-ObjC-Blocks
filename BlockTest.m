ef810427536aad95bb92b5d89de3ce8468e66064
Remy Demarest <remydemarest@Psycho-MacBook-Pro.local>
2009-04-30 21:32:16 +0200

    Corrected tests

--- BlockTest.m
+++ BlockTest.m
@@ -2,15 +2,15 @@
 #include "blocks_runtime.h"
 
 //#import <Cocoa/Cocoa.h>
-#if 0
-typedef int (^intblock)(int in);
+#if 1
+typedef int (^intblock)(int);
 
 @interface PBTest : NSObject
 {
     intblock onRelease;
     int num;
 }
-@property (retain) intblock onRelease;
+@property (copy) intblock onRelease;
 @property (assign) int num;
 @end
 
@@ -26,28 +26,25 @@ typedef int (^intblock)(int in);
 
 - (void)release
 {
-    intblock(num);
+    onRelease(num);
     [super release];
 }
-@end
 
-void func(void)
+- (void)dealloc
 {
-    int (^blk)(int i) = ^(int i){ printf("Returning 2 * %i", i); return 2*i; };
-    NSLog(@"%@", ((id)blk)->isa);
+    [onRelease release];
+    [super dealloc];
 }
+@end
 
 int main (int argc, char const *argv[])
 {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
     PBTest *a = [[PBTest alloc] init];
     a.num = 3;
-    int (^blk)(int i) = ^(int i){ printf("Returning 2 * %i", i); return 2*i; };
-    int (^blk2)(int i);
-    NSLog(@"%@", ((id)blk)->isa);
-    func();
-    
+    a.onRelease = ^(int i){ printf("Returning 2 * %i", i); return 2*i; };
     [a release];
+    
     [pool drain];
     return 0;
 }
@@ -103,6 +100,8 @@ void tryBlock(BLK b1, BLK b2)
 
 @end
 
+BLKI b = ^(int i){ NSLog(@"%d", i); };
+
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     // Pure C compatible part
@@ -120,8 +119,14 @@ int main (int argc, const char * argv[]) {
     BLKI blk = [obj selfBlock];
     blk(10);
     [obj superBlock](42);
-    NSLog(@"%@", ((id)blk)->isa);
-    [obj setOnRelease:^(int i){ NSLog(@"%d", i); }];
+    
+    BLKI b3 = ^(int i){ NSLog(@"%d", i); };
+    BLKI b4 = ^(int i){ NSLog(@"%d", i); };
+    
+    NSLog(@"%p, %p, %p, %p", (void *)b, b3, b4, main);
+    
+    NSLog(@"%@", ((id) ^(int i){ NSLog(@"%d", i); })->isa);
+    obj.onRelease = ^(int i){ NSLog(@"test %d", i); };
     
     //[blk release];
     //
